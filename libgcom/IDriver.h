ad6f370b35645fec2a8e743a0a28d616bb810e57
Jestin Stoffel <jestin.stoffel@gmail.com>
2011-04-11 16:13:29 -0500

    added license to files in libgcom

--- libgcom/IDriver.h
+++ libgcom/IDriver.h
@@ -1,3 +1,26 @@
+/*
+ IDriver.h
+
+ Interface definition for drivers.
+     
+ Part of the gcom project
+ Copyright (c) 2011 Jestin Stoffel
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #ifndef IDRIVER_H
 #define IDRIVER_H
 
--- libgcom/S3GPackage.h
+++ libgcom/S3GPackage.h
@@ -1,3 +1,27 @@
+/*
+ S3GPackage.h
+
+ Base class for Sanguino based motherboard command packages.  Might
+ also work for Arduino Mega based motherboards, but I haven't checked.
+     
+ Part of the gcom project
+ Copyright (c) 2011 Jestin Stoffel
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #ifndef S3GPACKAGE_H
 #define S3GPACKAGE_H
 
